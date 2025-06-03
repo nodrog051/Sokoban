d36163081d56432ffdf906b216100d0dc286a8b8
nodrog051 <gordon.dickson@gmail.com>
2025-06-03 17:24:32 +0100

    Updated

--- Globals/LevelData.gd
+++ Globals/LevelData.gd
@@ -1,11 +1,14 @@
 extends Node
+const LEVEL_DATA_PATH : String = "res://Data/level_data.json"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _enter_tree() -> void:
+	load_level_data()
+	
+func load_level_data() -> void:
+	var file : FileAccess = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
+	
+	if !file:
+		return
+		
+	var json_data: Dictionary = JSON.parse_string(file.get_as_text())
+		
